685b78fc4c00e6c951aec879d3f021fc621e373c
Gabriel Andrade <gabrielhbandrade@hotmail.com>
2023-01-30 16:39:25 +0900

    Point-wise annotation

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,9 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.NumberFormat;
 import java.util.Stack;
@@ -34,7 +36,7 @@ import javax.xml.parsers.ParserConfigurationException;
 
 public class Main {
 
-    public record Annotation(int start, int end, String tag)  {}
+    public record Annotation(int start, String tag) {}
 
     public static class TextAreaExample {
 
@@ -51,17 +53,19 @@ public class Main {
             JFrame f = new JFrame();
             area = new JTextArea(documents[i]);
             area.setEditable(false);
-            area.setBounds(10, 10, 600, 300);
+            area.setBounds(10, 10, 1000, 600);
             area.setLineWrap(true);
             area.setWrapStyleWord(true);
 
             JPanel panel1 = new JPanel();
+            FlowLayout layout = new FlowLayout();
+            layout.setHgap(layout.getHgap() + 50);
+            panel1.setLayout(layout);
             String[] options = {"C", "CN"};
             JComboBox<String> comboBox = new JComboBox<>(options);
             comboBox.setSelectedIndex(0);
             comboBox.setPreferredSize(new Dimension(100, 30));
             JLabel label = new JLabel("Document");
-
 
             NumberFormat format = NumberFormat.getInstance();
             NumberFormatter formatter = new NumberFormatter(format);
@@ -79,11 +83,16 @@ public class Main {
             textField.addActionListener(e -> {
                 changeText(Integer.parseInt(textField.getText()) - 1);
             });
-            panel1.add(comboBox);
-            panel1.add(label);
-            panel1.add(textField);
+            JPanel p1 = new JPanel();
+            p1.add(new JLabel("Tag:"));
+            p1.add(comboBox);
+            panel1.add(p1);
+            p1 = new JPanel();
+            p1.add(label);
+            p1.add(textField);
             label = new JLabel("of " + documents.length);
-            panel1.add(label);
+            p1.add(label);
+            panel1.add(p1);
 
             next = new JButton("Next");
             next.addActionListener(e -> {
@@ -104,35 +113,51 @@ public class Main {
                 changeText(i - 1);
             });
 
+            JPanel panel2 = new JPanel(new BorderLayout());
             JPanel panel = new JPanel(new FlowLayout());
             panel.add(prev);
-            panel.add(undo);
             panel.add(reset);
+            panel.add(undo);
             panel.add(next);
+            panel2.add(panel, BorderLayout.NORTH);
+            panel = new JPanel(new FlowLayout());
+            JButton save = new JButton("Save");
+            save.addActionListener(e -> {
+                JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+                jfc.setDialogTitle("Specify a file to save");
+                int returnValue = jfc.showSaveDialog(f);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = jfc.getSelectedFile();
+                    System.out.println(selectedFile.getAbsolutePath());
+                    saveXML(selectedFile.getAbsolutePath(), documents);
+                }
 
-            f.setSize(620, 320);
+            });
+            panel.add(save);
+            panel2.add(panel , BorderLayout.SOUTH);
+
+            f.setSize(1000, 600);
             f.setLayout(new BorderLayout());
             f.add(panel1, BorderLayout.PAGE_START);
             f.add(area, BorderLayout.CENTER);
-            f.add(panel, BorderLayout.PAGE_END);
+            f.add(panel2, BorderLayout.PAGE_END);
             f.setVisible(true);
+            area.setHighlighter(null);
             area.addMouseListener(new MouseAdapter() {
-                public void mouseReleased(MouseEvent e) {
+                public void mousePressed(MouseEvent e) {
 
                     if (e.getButton() == MouseEvent.BUTTON1) {
                         int start = area.getSelectionStart();
-                        int end = area.getSelectionEnd();
                         String text = area.getText();
 
-                        if (text.substring(start, end).strip().length() == 0) {
+                        if (start == area.getText().length()) {
                             return;
                         }
 
                         String tag = (String) comboBox.getSelectedItem();
-                        area.setText(text.substring(0, start) + "<" + tag + ">" +
-                                text.substring(start, end) + "</" + tag + ">" +
-                                text.substring(end));
-                        annotations.push(new Annotation(start, end, tag));
+                        area.setText(text.substring(0, start) + "<" + tag + " />" +
+                                text.substring(start));
+                        annotations.push(new Annotation(start, tag));
                     }
                 }
             });
@@ -152,39 +177,39 @@ public class Main {
                 public void changedUpdate(DocumentEvent arg0) {
                     documents[i] = area.getText();
                 }
-            });        }
+            });
+        }
 
-        private void undo(){
+        private void undo() {
             if (!annotations.isEmpty()) {
                 Annotation annotation = annotations.pop();
-                area.replaceRange("", annotation.start, annotation.start + annotation.tag.length() + 2);
-                area.replaceRange("", annotation.end, annotation.end + annotation.tag.length() + 3);
+                area.replaceRange("", annotation.start,
+                        annotation.start + annotation.tag.length() + 4);
             }
         }
 
-        private void changeText(int newDoc){
+        private void changeText(int newDoc) {
             if (newDoc > documents.length - 1 || newDoc < 0) {
                 return;
             }
             i = newDoc;
+            annotations.clear();
             area.setText(documents[i]);
             textField.setText(String.valueOf(i + 1));
             if (i == 0) {
                 prev.setEnabled(false);
-            }
-            else {
+            } else {
                 prev.setEnabled(true);
             }
 
             if (i == documents.length - 1) {
                 next.setEnabled(false);
-            }
-            else {
+            } else {
                 next.setEnabled(true);
             }
         }
 
-        private static String[] readXML(String path){
+        private static String[] readXML(String path) {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             try {
                 dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
@@ -210,10 +235,28 @@ public class Main {
             return null;
         }
 
+        private static void saveXML(String path, String[] documents){
+            File file = new File(path);
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+                writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+                writer.write("<articles>\n");
+                for (String document : documents) {
+                    writer.write("<article>\n");
+                    writer.write(document + "\n");
+                    writer.write("</article>\n");
+                }
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
 
 
-    public static String fileChooser(){
+    public static String fileChooser() {
         JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
         jfc.setFileFilter(new FileNameExtensionFilter("Text files", "txt", "xml"));
         jfc.setAcceptAllFileFilterUsed(false);
